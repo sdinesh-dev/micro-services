a01548e52e4cf7f9a4b39b6a58f84609f70164a1
Dineshkumar S <sdineshcbe@gmail.com>
2023-09-14 22:52:13 +0530

    Hystrix Property Configuration

--- movie-catalog-service/src/main/java/com/dinesh/moviecatalogservice/services/MovieService.java
+++ movie-catalog-service/src/main/java/com/dinesh/moviecatalogservice/services/MovieService.java
@@ -3,6 +3,7 @@ package com.dinesh.moviecatalogservice.services;
 import com.dinesh.moviecatalogservice.models.Movie;
 import com.dinesh.moviecatalogservice.models.Rating;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,11 +13,16 @@ public class MovieService {
     @Autowired
     RestTemplate restTemplate;
 
-    @HystrixCommand(fallbackMethod = "getFallbackMovie")
+    @HystrixCommand(fallbackMethod = "getFallbackMovie",
+            commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds", value="2000"),
+            @HystrixProperty(name="circuitBreaker.requestVolumeThreshold", value="5"),
+            @HystrixProperty(name="circuitBreaker.errorThresholdPercentage", value="50"),
+            @HystrixProperty(name="circuitBreaker.sleepWindowInMilliseconds", value="5000")
+    })
     public Movie getMovie(Rating rating){
         return restTemplate.getForObject("http://movie-info-service/movies/"+rating.getMovieId(), Movie.class);
     }
-
     public Movie getFallbackMovie(Rating rating){
         return new Movie("MovieId","Movie name not found","Movie Description Not Found");
     }
