47d6dcb8e6228f5a8ad39ea01c3993dc6f8b66de
Dineshkumar S <sdineshcbe@gmail.com>
2023-09-22 22:23:38 +0530

    Added YAML and Spring Active Profiles

--- spring-boot-config/src/main/java/com/dinesh/microservices/config/controller/ConfigController.java
+++ spring-boot-config/src/main/java/com/dinesh/microservices/config/controller/ConfigController.java
@@ -2,6 +2,7 @@ package com.dinesh.microservices.config.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -26,7 +27,10 @@ public class ConfigController {
     private Map<String, String> mapValues;
 
     @Autowired
-    DbSettings dbSettings;
+    private DbSettings dbSettings;
+
+    @Autowired
+    private Environment env;
 
     @GetMapping("/greeting")
     public String getGreeting(){
@@ -57,4 +61,10 @@ public class ConfigController {
     public String getDbSettings(){
         return dbSettings.getConnection() + dbSettings.getHost() + dbSettings.getPort();
     }
+
+    @GetMapping("/envdetails")
+    public String getEnvDetails(){
+        return env.toString();
+    }
+
 }
