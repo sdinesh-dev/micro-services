a952e2d6e4b8b417efd52e7c945478fea0bd2eeb
Dineshkumar S <sdineshcbe@gmail.com>
2023-09-23 15:59:54 +0530

    Dynamic Config Refresh

--- spring-boot-config/src/main/java/com/dinesh/microservices/config/controller/ConfigController.java
+++ spring-boot-config/src/main/java/com/dinesh/microservices/config/controller/ConfigController.java
@@ -2,6 +2,7 @@ package com.dinesh.microservices.config.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@RefreshScope
 public class ConfigController {
 
     @Value("${my.greeting}")
